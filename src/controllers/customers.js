fc2756937da8d77a857974e4e38f5861d77348c1
Brian Villanova <brian.rj99@yahoo.com.br>
2021-11-15 19:40:47 +0000

    listing clients and create new partials file

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,6 +1,14 @@
 const CustomersModel = require('../models/customers')
 const { crypto } = require('../utils/password')
 
+
+const defaultTitle = 'Cadastro de Clientes'
+function index(req, res) {
+    res.render("register", {
+        title: "Cadastro de clientes"
+    })
+}
+
 async function add(req, res) {
     const {
         name,
@@ -19,10 +27,26 @@ async function add(req, res) {
     })
 
     register.save()
-    res.send('cadastro realizado!')
+    res.render('register', {
+        title: defaultTitle,
+        message: 'Cadastro realizado com sucesso'
+
+    })
 
 }
 
+async function listUsers(req, res) {
+   const users = await CustomersModel.find() // find vazio chama todos os users do banco de dados
+
+    res.render('listUsers', {
+        title: 'Listagem de Usuários',
+        users,
+    })
+}
+
+
 module.exports = {
-    add
+    add,
+    index,
+    listUsers,
 }
